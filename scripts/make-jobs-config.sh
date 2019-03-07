78cc35c5001757a9b66b45ce7568396ad66d0b7b
Joel Speed <Joel.speed@hotmail.co.uk>
2019-03-07 14:13:49 +0000

    Add config generation, verification, mount config, plugins

---
+++ scripts/make-jobs-config.sh
@@ -0,0 +1,9 @@
+#/env/bin/bash
+
+config=""
+for f in $(ls config/jobs/*/*.yaml); do
+  k=${f#"config/jobs/"}
+  k=$(echo $k | sed "s|/|_|")
+  config="$config --from-file=$k=$f"
+done
+kubectl create configmap jobs ${config} --dry-run -o yaml
